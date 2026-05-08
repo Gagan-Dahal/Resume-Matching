d63f97468c01549f20fbb9c9e049924d13e79689
Gagan-Dahal <gauravdahal222@gmail.com>
2026-05-08 21:11:40 +0545

    Predefined section keys and experience extraction using regex

---
+++ extract_experience.py
@@ -0,0 +1,90 @@
+import spacy
+import json
+import re
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+EARLIEST_JOB_START = datetime.now().year - 50
+CURRENT_DATETIME = datetime.now()
+
+month = r"(?:january|february|march|april|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|0?[1-9]|1[0-2])"
+year = r"\d{4}"
+date_separators = r"[-/.]"
+
+start_patterns = rf"""(?:
+    {month}[ \t]*{date_separators}[ \t]*{year}
+    |{year}[ \t]*{date_separators}[ \t]*{month}
+    |{month}[ \t]*{year}
+    |{year}[ \t]*{month}
+    |(?<!\d){year}(?!\d)
+)"""
+
+current_words = r"(?:now|present|today|current)"
+end_patterns = rf"(?:{start_patterns}|{current_words})"
+
+range_separators = r"(?:-|to)"
+
+range_patterns = rf"""(?:
+    {start_patterns}[ \t]+{range_separators}[ \t]+{end_patterns}
+)"""
+
+
+conj_pat = re.compile(range_patterns, re.IGNORECASE|re.VERBOSE)
+
+with open("demo.json", "r") as fp:
+    sectioned_database = json.load(fp)
+
+demo_text = sectioned_database["experience"]
+
+# demo_text = "Worked as Senior developer in Amazon from 2020 to 2024\n MAY 2020falkjflk, 2020 December - present\n 02/2020 03/2025"
+finds = re.findall(string = demo_text, pattern = conj_pat)
+print(finds)
+
+
+def parse_date(date_str:str):
+    if not isinstance(date_str, str):
+        raise TypeError(f"{type(date_str)} sent instead of str")
+    if re.search(current_words, date_str, re.IGNORECASE):
+        return datetime.now()
+    try:
+        parsed_date = parser.parse(date_str, fuzzy=True)
+        if parsed_date.year >= EARLIEST_JOB_START and parsed_date.year<=CURRENT_DATETIME.year:
+            return parsed_date
+        else:
+            return None
+    except:
+        return None
+    
+
+def calculate_experience(range_list:list):
+    if not isinstance(range_list, list):
+        raise TypeError(f"{type(range_list)} sent instead of list")
+    total_experience_months = 0
+    for range in range_list:
+        dates = re.split(re.compile(rf"(?:[ \t]*{range_separators}[ \t]*|[ \t]+)"), range, maxsplit=1)
+        if len(dates) == 2:
+            start_date = parse_date(dates[0])
+            end_date = parse_date(dates[1])
+            if not start_date or not end_date:
+                continue
+            if start_date > end_date:
+                start_date, end_date = end_date, start_date
+
+            print(f"{start_date.year}-{start_date.month}, {end_date.year}-{end_date.month}")
+            delta = relativedelta(end_date, start_date)
+            print(delta)
+            total_experience_months += delta.years * 12 + delta.months
+    return (total_experience_months // 12, total_experience_months % 12)
+
+print(calculate_experience(finds))
+
+# nlp = spacy.load("en_core_web_lg")
+# sectioned_database = ""
+
+
+
+# doc = nlp(experience_raw.lower())
+
+# for ent in doc.ents:
+#     print(ent.text, "->", ent.label_)
--- section_page.py
+++ section_page.py
@@ -4,7 +4,7 @@ import json
 SECTION_HEADERS = {
     "summary": ["summary", "professional summary", "profile", "objective", "introduction", "additional information"],
     "skills": ["skills", "technical skills", "core competencies"],
-    "education": ["education", "educational qualification", "qualification", "academic background"],
+    "education": ["education", "educational qualification", "qualification", "academic background", "education and training"],
     "certifications": ["certifications", "certificates", "licenses", "training"],
     "experience": ["experience", "work experience", "professional experience", "employment history"],
     "projects": ["projects", "academic projects", "side projects"],
@@ -28,11 +28,16 @@ for index, match in enumerate(matches):
     # print(match.start(), match.end(), match.groups(), match.group(0), match.group(1))
     text_start = match.end()
     text_end = matches[index+1].start() if index < len(matches) - 1 else len(test_text)
-
-    sectioned_text[match.group(1).lower()] = test_text[text_start: text_end]
+    extracted_topic = match.group(1).lower()
+    normalized_topic = extracted_topic if extracted_topic in SECTION_HEADERS.keys() else None
+    if normalized_topic is None:
+        for key, value in SECTION_HEADERS.items():
+            if extracted_topic in value:
+                normalized_topic = key
+    sectioned_text[normalized_topic] = test_text[text_start: text_end]
 
 # print(sectioned_text)
 
-# with open("demo.json", "w") as fp:
+with open("demo.json", "w") as fp:
 
-#     json.dump(sectioned_text, fp, indent=4)
+    json.dump(sectioned_text, fp, indent=4)
