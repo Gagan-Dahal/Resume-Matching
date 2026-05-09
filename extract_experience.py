726cfaa6a65cc1a124292641c814bd6254786759
Gagan-Dahal <gauravdahal222@gmail.com>
2026-05-09 18:41:39 +0545

    Refined experience extraction

--- extract_experience.py
+++ extract_experience.py
@@ -11,7 +11,6 @@ CURRENT_DATETIME = datetime.now()
 month = r"(?:january|february|march|april|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|0?[1-9]|1[0-2])"
 year = r"\d{4}"
 date_separators = r"[-/.]"
-
 start_patterns = rf"""(?:
     {month}[ \t]*{date_separators}[ \t]*{year}
     |{year}[ \t]*{date_separators}[ \t]*{month}
@@ -19,27 +18,18 @@ start_patterns = rf"""(?:
     |{year}[ \t]*{month}
     |(?<!\d){year}(?!\d)
 )"""
-
 current_words = r"(?:now|present|today|current)"
 end_patterns = rf"(?:{start_patterns}|{current_words})"
 
-range_separators = r"(?:-|to)"
 
+range_separators = r"(?:-|to)"
 range_patterns = rf"""(?:
     {start_patterns}[ \t]+{range_separators}[ \t]+{end_patterns}
 )"""
-
-
 conj_pat = re.compile(range_patterns, re.IGNORECASE|re.VERBOSE)
 
-with open("demo.json", "r") as fp:
-    sectioned_database = json.load(fp)
 
-demo_text = sectioned_database["experience"]
-
-# demo_text = "Worked as Senior developer in Amazon from 2020 to 2024\n MAY 2020falkjflk, 2020 December - present\n 02/2020 03/2025"
-finds = re.findall(string = demo_text, pattern = conj_pat)
-print(finds)
+experience_pattern = r"(\d{1,2})\+?[ \t]+(?:year|years)[ \t]*(?:of[ \t]+experience|experience)?"
 
 
 def parse_date(date_str:str):
@@ -60,9 +50,11 @@ def parse_date(date_str:str):
 def calculate_experience(range_list:list):
     if not isinstance(range_list, list):
         raise TypeError(f"{type(range_list)} sent instead of list")
-    total_experience_months = 0
-    for range in range_list:
-        dates = re.split(re.compile(rf"(?:[ \t]*{range_separators}[ \t]*|[ \t]+)"), range, maxsplit=1)
+    
+    parsed_ranges = []
+
+    for list_range in range_list:
+        dates = re.split(re.compile(rf"[ \t]*{range_separators}[ \t]*"), list_range, maxsplit=1)
         if len(dates) == 2:
             start_date = parse_date(dates[0])
             end_date = parse_date(dates[1])
@@ -71,13 +63,57 @@ def calculate_experience(range_list:list):
             if start_date > end_date:
                 start_date, end_date = end_date, start_date
 
-            print(f"{start_date.year}-{start_date.month}, {end_date.year}-{end_date.month}")
-            delta = relativedelta(end_date, start_date)
-            print(delta)
-            total_experience_months += delta.years * 12 + delta.months
-    return (total_experience_months // 12, total_experience_months % 12)
 
-print(calculate_experience(finds))
+            print(f"{start_date.year}-{start_date.month}, {end_date.year}-{end_date.month}")
+            parsed_ranges.append([start_date, end_date])
+            
+    parsed_ranges.sort(key = lambda x: x[0])
+
+    total_experience_months = 0
+    list_index = 0
+    while list_index < (len(parsed_ranges) - 1):
+        cur = parsed_ranges[list_index]
+        nex = parsed_ranges[list_index+1]
+
+        if cur[1] >= nex[0]:
+            cur[1] = max(cur[1], nex[1])
+            parsed_ranges.pop(list_index + 1)
+        else:
+            list_index += 1
+
+    for parsed_range in parsed_ranges:
+        start_date, end_date = parsed_range
+        delta = relativedelta(end_date, start_date)
+        print(delta)
+        total_experience_months += delta.years * 12 + delta.months
+
+    return total_experience_months // 12
+
+
+def extract_experience(experience_text):
+    matches = re.search(experience_pattern, experience_text, re.IGNORECASE)
+    if matches:
+        print("String Match")
+        years = 0
+        matches = re.finditer(experience_pattern, experience_text, re.IGNORECASE)
+        for match in matches:
+            years += int(match.group(1))
+        return years
+    else:
+        print("Trying date match")
+        finds = re.findall(string = experience_text, pattern = conj_pat)
+        return calculate_experience(finds)
+
+
+with open("test.json", "r") as fp:
+    sectioned_database = json.load(fp)
+
+demo_text = sectioned_database["experience"]
+print(demo_text)
+print(extract_experience(demo_text))
+
+
+# print(calculate_experience(finds))
 
 # nlp = spacy.load("en_core_web_lg")
 # sectioned_database = ""
